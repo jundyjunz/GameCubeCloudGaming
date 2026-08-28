a434c71f96e3e74a54d3205d7710c839447bd611
Ryan Irwandy <rjirwandy@gmail.com>
2026-08-27 23:57:20 -0700

    added linux build

--- GameCubeOnline/GameCubeOnline/obj/Debug/net10.0/linux-x64/GameCubeOnline.AssemblyInfo.cs
+++ GameCubeOnline/GameCubeOnline/obj/Debug/net10.0/linux-x64/GameCubeOnline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameCubeOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f381adb605d66d03eec703223b13e6b482b7a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf34fbdf872f562d3717b392ef53c01afc6c1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameCubeOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameCubeOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
